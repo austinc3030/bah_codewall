f4639ebb55cf39ebe4f370ecfe74fa08a38e40e7
austinc3030 <austinc3030@gmail.com>
2019-05-23 19:31:17 -0400

    Code formatting and initial comments for java

--- 6_java.java
+++ 6_java.java
@@ -8,46 +8,37 @@ public class Decrypt
         int     m       = 9; 
         char[ ] result  = new char[ m ]; 
         
-        for ( int  i = 1;  i < n / 2; i++ ) 
-        { 
+        for ( int  i = 1;  i < n / 2; i++ ) { 
+     
             result[ i ] = ( char ) ( key.charAt( i + 2 ) ); 
-        } 
+     
+        } // End for
         
-        for ( int i = 0; i < ( n - 1 ); i++ )
-        {
+        for ( int i = 0; i < ( n - 1 ); i++ ) {
 
-            if ( i == 0 ) 
-            {
+            if ( i == 0 ) {
             
                 result[ i ] = ( char ) ( key.charAt( i ) + ( i + 2 ) ); 
             
-            } 
-            else if ( i == ( n - 3 ) ) 
-            { 
+            } else if ( i == ( n - 3 ) ) { 
             
                 result[ i ] = ( char ) ( key.charAt( i + 2 ) + i ); 
             
-            } 
-            else if ( i == ( n - 2 ) ) 
-            { 
+            } else if ( i == ( n - 2 ) ) { 
             
                 result[ i ] = ( char ) ( key.charAt( i - 4 ) + ( i - 1 ) ); 
             
-            } 
+            } // End ifelse
         
-        } 
+        } // End for
         
-        for ( int i = ( n - 1 ); i < m; i++ ) 
-        { 
+        for ( int i = ( n - 1 ); i < m; i++ ) { 
             
-            if ( i == ( n - 1 ) || i == ( m - 1 ) ) 
-            { 
+            if ( i == ( n - 1 ) || i == ( m - 1 ) ) { 
             
                 result[ i ] = ( char ) ( key.charAt( 0 ) - ( n - 2 ) ); 
             
-            } 
-            else if ( i == n ) 
-            { 
+            } else if ( i == n ) { 
             
                 result[ i ]= ( char ) ( key.charAt( 0 ) - ( n - 1 ) ); 
             
@@ -55,15 +46,15 @@ public class Decrypt
             
                 result[ i ] = ( char ) ( key.charAt( 0 ) - ( i - 5 ) ); 
             
-            } 
+            } // End ifelse 
 
-        } 
+        } // End for
         
         String joined = new String( result );
 
         System.out.println( joined ); 
         decrypt_javascript( joined ); 
     
-    } 
+    } // End decrypt_java
 
-}
+} // End Decrypt

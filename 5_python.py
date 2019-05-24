c80ae940f5bfb69b176774b12ad3e525265ff4e5
austinc3030 <austinc3030@gmail.com>
2019-05-23 20:15:21 -0400

    Initial comments and code formatting for Python

--- 5_python.py
+++ 5_python.py
@@ -4,13 +4,23 @@ def decrypt_python( key ):
     space  = " "
 
     for i in range( 0, 3 ):
+
         if i == 0: 
+
             result += key[ i ] + chr( ord( key[ i + 2 ] ) - 1 ) 
+
         elif i == 1:
+
             result += space + key[ i ]
+
         else:
+
             result += chr( ord( key[ i - 1 ] ) + 1 ) + key[ i ] 
+
+        # End for
 
     print( result )
     
     decrypt_java( result )
+
+# End decrypt_python( )

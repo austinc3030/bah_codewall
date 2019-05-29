4677e3d2fb21202966bfc4d2418104f9a254d476
austinc3030 <austinc3030@gmail.com>
2019-05-29 18:01:02 -0400

    Fixed code formatting for GO

--- 8_GO.go
+++ 8_GO.go
@@ -1,32 +1,30 @@
-func decrypt_go( inp string ) string 
-{ 
-    
-    final := make( [ ]rune, 12 )
-    d := "\x0b\x0f\x03\x05\x1c\x1b\x14\x01\x1d\x1b\x1a"
-    
-    if len( inp ) != 4 
-    { 
-    
-        return "" 
-    
-    } // End if
-    
-    var cnt int = -1
+func decrypt_go( inp string ) string {
 
-    for i, l := range d 
-    {  
-    
-        if i % len( inp ) == 0 
-        {  
-    
-            cnt++  
-    
-        } // End if
-        
-        final[ i ] = rune( inp[ cnt ] ) ^ l 
-    
-    } // End for
-        
-    fmt.Println( string( final ) )  
+	final := make( [ ]rune, 12 )
+	d := "\x0b\x0f\x03\x05\x1c\x1b\x14\x01\x1d\x1b\x1a"
+
+	if len( inp ) != 4 {
+
+	return ""
+
+	} // End if
+
+	var cnt int = -1
+
+	for i, l := range d {
+
+		if i % len( inp ) == 0 {
+
+		cnt++
+
+		} // End if
+
+		final[ i ] = rune( inp[ cnt ] ) ^ l
+
+	} // End for
+
+	fmt.Println( string( final ) )
+	
+	return string( final )
 
 } // End decrypt_go( )
